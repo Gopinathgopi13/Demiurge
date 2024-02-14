7de0a4cf98476e272534f4b120122e7a6fa1b449
Gopinathgopi13 <gopinathkathirvel13@gmail.com>
2024-02-14 17:44:18 +0530

    commit

--- src/Components/About/OurTeam.jsx
+++ src/Components/About/OurTeam.jsx
@@ -1,10 +1,12 @@
-import React from "react";
 
 function OurTeam() {
   return (
     <div className="w-full">
-      <div className="lg:w-[1280px] mx-auto px-4 lg:pb-20 h-[90vh] pt-44">
-        <div>Our team</div>
+      <div className="lg:w-[1280px] mx-auto px-4 py-10">
+        <div className="text-4xl">Our team</div>
+        <div>
+          
+        </div>
       </div>
     </div>
   );
