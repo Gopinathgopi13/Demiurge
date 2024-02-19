5d288da6d953ae75616d33c738d2e3f643189b31
Gopinathgopi13 <gopinathkathirvel13@gmail.com>
2024-02-19 10:15:28 +0530

    commit

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { PopupButton } from "react-calendly";
 
 const Button = () => {
   return (
-    <button className="text-white  px-6 py-2 hover:bg-blue-700 border-2 hover:border-none">
-      Book Intro Call
-    </button>
+    <PopupButton
+      className="text-white  px-6 py-2 hover:bg-blue-700 border-2 hover:border-none"
+      url="https://calendly.com/gopinathkathirvel13"
+      rootElement={document.getElementById("root")}
+      text="Schedule a Call"
+    />
   );
 };
 
