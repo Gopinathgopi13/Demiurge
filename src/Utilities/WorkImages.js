e786fe15a6c38be0530d87d46eb8e1f8085ec937
Gopinathgopi13 <gopinathkathirvel13@gmail.com>
2024-02-27 23:55:15 +0530

    scroll animation

--- src/Utilities/WorkImages.js
+++ src/Utilities/WorkImages.js
@@ -1,6 +1,21 @@
 import oluntier1 from "../../public/static//Works/oluntier1.jpg";
 import oluntier2 from "../../public/static//Works/oluntier2.jpg";
 import oluntier3 from "../../public/static//Works/oluntier3.jpg";
+import faywalk1 from "../../public/static//Works/faywalk1.jpg";
+import faywalk2 from "../../public/static//Works/faywalk2.jpg";
+import faywalk3 from "../../public/static//Works/faywalk3.jpg";
+import me1 from "../../public/static//Works/me1.webp";
+import me2 from "../../public/static//Works/me2.webp";
+import me3 from "../../public/static//Works/me3.webp";
+import melba1 from "../../public/static//Works/melba1.webp";
+import melba2 from "../../public/static//Works/melba2.webp";
+import melba3 from "../../public/static//Works/melba3.webp";
+import melba4 from "../../public/static//Works/melba4.webp";
+import melba5 from "../../public/static//Works/melba5.webp";
+import melba6 from "../../public/static//Works/melba6.webp";
+import melba7 from "../../public/static//Works/melba7.webp";
+import melba8 from "../../public/static//Works/melba8.webp";
+import melba9 from "../../public/static//Works/melba9.webp";
 
 let data = [
   {
@@ -17,63 +32,63 @@ let data = [
   },
   {
     id: 4,
-    img: "https://raw.githubusercontent.com/Gopinathgopi13/Demiurge/main/public/Works/faywalk1.jpg",
+    img: faywalk1,
   },
   {
     id: 5,
-    img: "https://raw.githubusercontent.com/Gopinathgopi13/Demiurge/main/public/Works/faywalk2.jpg",
+    img: faywalk2,
   },
   {
     id: 6,
-    img: "https://raw.githubusercontent.com/Gopinathgopi13/Demiurge/main/public/Works/faywalk3.jpg",
+    img: faywalk3,
   },
   {
     id: 7,
-    img: "https://raw.githubusercontent.com/Gopinathgopi13/Demiurge/main/public/Works/me1.webp",
+    img: me1,
   },
   {
     id: 8,
-    img: "https://raw.githubusercontent.com/Gopinathgopi13/Demiurge/main/public/Works/me2.webp",
+    img: me2,
   },
   {
     id: 9,
-    img: "https://raw.githubusercontent.com/Gopinathgopi13/Demiurge/main/public/Works/me3.webp",
+    img: me3,
   },
   {
     id: 10,
-    img: "https://raw.githubusercontent.com/Gopinathgopi13/Demiurge/main/public/Works/melba1.webp",
+    img: melba1,
   },
   {
     id: 11,
-    img: "https://raw.githubusercontent.com/Gopinathgopi13/Demiurge/main/public/Works/melba2.webp",
+    img: melba2,
   },
   {
     id: 12,
-    img: "https://raw.githubusercontent.com/Gopinathgopi13/Demiurge/main/public/Works/melba3.webp",
+    img: melba3,
   },
   {
     id: 13,
-    img: "https://raw.githubusercontent.com/Gopinathgopi13/Demiurge/main/public/Works/melba4.webp",
+    img: melba4,
   },
   {
     id: 14,
-    img: "https://raw.githubusercontent.com/Gopinathgopi13/Demiurge/main/public/Works/melba5.webp",
+    img: melba5,
   },
   {
     id: 15,
-    img: "https://raw.githubusercontent.com/Gopinathgopi13/Demiurge/main/public/Works/melba6.webp",
+    img: melba6,
   },
   {
     id: 16,
-    img: "https://raw.githubusercontent.com/Gopinathgopi13/Demiurge/main/public/Works/melba7.webp",
+    img: melba7,
   },
   {
     id: 17,
-    img: "https://raw.githubusercontent.com/Gopinathgopi13/Demiurge/main/public/Works/melba8.webp",
+    img: melba8,
   },
   {
     id: 18,
-    img: "https://raw.githubusercontent.com/Gopinathgopi13/Demiurge/main/public/Works/melba9.webp",
+    img: melba9,
   },
 ];
 
