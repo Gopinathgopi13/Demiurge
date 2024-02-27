e4b59f2e8731e107485a101f587c9e22b007d223
gopinathgopi13 <gopinathkathirvel13@gmail.com>
2024-02-27 18:51:48 +0530

    loader

--- src/Components/Home/Clients/Clients.jsx
+++ src/Components/Home/Clients/Clients.jsx
@@ -1,16 +1,102 @@
 import React from "react";
-import ClientLogo from "./ClientLogo";
-import clientLogo from "../../../Utilities/ClientLogos";
+// import ClientLogo from "./ClientLogo";
+// import clientLogo from "../../../Utilities/ClientLogos";
+import cl1 from "../../../assets/client/cl1.webp";
+import cl2 from "../../../assets/client/cl2.webp";
+import cl3 from "../../../assets/client/cl3.webp";
+import cl4 from "../../../assets/client/cl4.webp";
+import cl5 from "../../../assets/client/cl5.webp";
+import cl6 from "../../../assets/client/cl6.webp";
+import cl7 from "../../../assets/client/cl7.webp";
+import cl8 from "../../../assets/client/cl8.webp";
+import cl9 from "../../../assets/client/cl9.webp";
+import cl10 from "../../../assets/client/cl10.webp";
+import cl11 from "../../../assets/client/cl11.webp";
+import cl12 from "../../../assets/client/cl12.webp";
+import Marquee from "react-fast-marquee";
+// import cl13 from "../../../assets/client/cl13.webp";
 function Clients() {
   return (
-    <div className="w-full flex flex-col justify-center items-center">
-      <div className="lg:w-[1280px]">
-        <div className=" grid lg:grid-cols-6 grid-cols-3 lg:gap-8">
-          {clientLogo.map(({ id, logo }) => {
-            return <ClientLogo key={id} logo={logo} />;
-          })}
-        </div>
+    <div className="lg:w-[1280px] mx-auto">
+      <div className="lg:text-6xl text-3xl font-bold text-[#e48f45] text-center">
+        Our Clients
       </div>
+      <Marquee>
+        <div>
+          <img src={cl1} alt="" />
+        </div>
+        <div>
+          <img src={cl2} alt="" />
+        </div>
+        <div>
+          <img src={cl3} alt="" />
+        </div>
+        <div>
+          <img src={cl4} alt="" />
+        </div>
+        <div>
+          <img src={cl5} alt="" />
+        </div>
+        <div>
+          <img src={cl6} alt="" />
+        </div>
+        <div>
+          <img src={cl7} alt="" />
+        </div>
+        <div>
+          <img src={cl8} alt="" />
+        </div>
+        <div>
+          <img src={cl9} alt="" />
+        </div>
+        <div>
+          <img src={cl10} alt="" />
+        </div>
+        <div>
+          <img src={cl11} alt="" />
+        </div>
+        <div>
+          <img src={cl12} alt="" />
+        </div>
+      </Marquee>
+      <Marquee direction="right">
+        <div>
+          <img src={cl12} alt="" />
+        </div>
+        <div>
+          <img src={cl11} alt="" />
+        </div>
+        <div>
+          <img src={cl10} alt="" />
+        </div>
+        <div>
+          <img src={cl9} alt="" />
+        </div>
+        <div>
+          <img src={cl8} alt="" />
+        </div>
+        <div>
+          <img src={cl7} alt="" />
+        </div>
+        <div>
+          <img src={cl6} alt="" />
+        </div>
+        <div>
+          <img src={cl5} alt="" />
+        </div>
+        <div>
+          <img src={cl4} alt="" />
+        </div>
+        <div>
+          <img src={cl3} alt="" />
+        </div>
+        <div>
+          <img src={cl2} alt="" />
+        </div>
+        <div>
+          <img src={cl1} alt="" />
+        </div>
+      </Marquee>
     </div>
   );
 }
