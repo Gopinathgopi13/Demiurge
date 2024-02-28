fbfc864890debf2c618b5b3e68da837bcb01b145
gopinathgopi13 <gopinathkathirvel13@gmail.com>
2024-02-28 10:51:47 +0530

    Calendly

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -5,7 +5,7 @@ const Button = () => {
   return (
     <PopupButton
       className="text-[#E48F45]  px-6 py-2 hover:bg-[#E48F45] hover:text-white border-2 border-[#E48F45] hover:border-none"
-      url="https://calendly.com/gopinathkathirvel13"
+      url="https://calendly.com/philomenjohn/digital-marketing-collab"
       rootElement={document.getElementById("root")}
       text="Schedule a Call"
     />
