cc1a8e7b4c862acc1bc0677ba20389239740f7d9
gopinathgopi13 <gopinathkathirvel13@gmail.com>
2024-03-08 15:02:32 +0530

    ALignment Changed

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ import Blockchain from "./Components/Service/Service_Items/Blockchain Developmen
 import LeadGeneration from "./Components/Service/Service_Items/Lead Generation/LeadGeneration";
 import ARVR from "./Components/Service/Service_Items/ARVR/ARVR";
 import Portfolio from "./Components/Portfolio/Portfolio";
-
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -36,34 +36,38 @@ function App() {
   return (
     !loading && (
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/work" element={<Work />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/careers" element={<Careers />} />
+        <ScrollToTop>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/work" element={<Work />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/careers" element={<Careers />} />
 
-          {/* Service */}
-          <Route path="/service" element={<Service />} />
+            {/* Service */}
+            <Route path="/service" element={<Service />} />
 
-          <Route path="/service/socialMedia" element={<SocialMedia />} />
-          <Route path="/service/seo" element={<SEO />} />
-          <Route path="/service/ppc" element={<PPC />} />
-          <Route path="/service/paidsocial" element={<PaidSocial />} />
-          <Route path="/service/pr" element={<PR />} />
-          <Route path="/service/video-marketing" element={<VideoMarketing />} />
-          <Route path="/service/brand" element={<Branding />} />
-          <Route path="/service/community" element={<Community />} />
-          <Route path="/service/web" element={<Web />} />
-          <Route path="/service/cgi" element={<CGI />} />
-          <Route path="/service/mobile" element={<MobileApp />} />
-          <Route path="/service/blockchain" element={<Blockchain />} />
-          <Route path="/service/lead" element={<LeadGeneration />} />
-          <Route path="/service/arvr" element={<ARVR />} />
+            <Route path="/service/socialMedia" element={<SocialMedia />} />
+            <Route path="/service/seo" element={<SEO />} />
+            <Route path="/service/ppc" element={<PPC />} />
+            <Route path="/service/paidsocial" element={<PaidSocial />} />
+            <Route path="/service/pr" element={<PR />} />
+            <Route
+              path="/service/video-marketing"
+              element={<VideoMarketing />}
+            />
+            <Route path="/service/brand" element={<Branding />} />
+            <Route path="/service/community" element={<Community />} />
+            <Route path="/service/web" element={<Web />} />
+            <Route path="/service/cgi" element={<CGI />} />
+            <Route path="/service/mobile" element={<MobileApp />} />
+            <Route path="/service/blockchain" element={<Blockchain />} />
+            <Route path="/service/lead" element={<LeadGeneration />} />
+            <Route path="/service/arvr" element={<ARVR />} />
 
-          {/* Industries */}
-          {/* <Route path="/industries" element={<Industries />} />
+            {/* Industries */}
+            {/* <Route path="/industries" element={<Industries />} />
           <Route path="/industries/fintech" element={<FintechMarketing />} />
           <Route path="/industries/crypto" element={<CryptoMarketing />} />
           <Route path="/industries/software" element={<SoftwareMarketing />} />
@@ -83,7 +87,8 @@ function App() {
           <Route path="/industries/healthcare" element={<Healthcare />} />
           <Route path="/industries/b2b" element={<B2bMarketing />} />
           <Route path="/industries/education" element={<Education />} /> */}
-        </Routes>
+          </Routes>
+        </ScrollToTop>
       </BrowserRouter>
     )
   );
